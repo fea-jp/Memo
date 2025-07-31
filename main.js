6915619b159f50940b9ee72997a10bef7998c796
ふぇあ <chitoseame.jp@gmail.com>
2025-07-31 22:50:26 +0900

    タッチキーボードの検出コードのテストを追加

--- main.js
+++ main.js
@@ -2,7 +2,9 @@ let initial_hight=window.innerHeight;
 
 window.addEventListener("resize",()=>{
     if(initial_hight<window.innerHeight){
+        alert("タッチキーボードを検出しました！")
     }else {
         initial_hight=window.innerHeight;
+        alert("タッチキーボードがなくなりました！")
     }
 });
