cf4f6117cb5a6a9d4b86ed186532742ee316e4c1
ふぇあ <chitoseame.jp@gmail.com>
2025-07-31 22:32:25 +0900

    initial commit

---
+++ main.js
@@ -0,0 +1,8 @@
+let initial_hight=window.innerHeight;
+
+window.addEventListener("resize",()=>{
+    if(initial_hight<window.innerHeight){
+    }else {
+        initial_hight=window.innerHeight;
+    }
+});
