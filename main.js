12677599625b1501b64f53f1f941b5e8d78df53c
ふぇあ <chitoseame.jp@gmail.com>
2025-07-31 23:04:38 +0900

    入力アシストのスタイルの追加、タッチキーボード検出コードの編集

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
 let initial_hight=window.innerHeight;
+const assist_box=document.getElementById("assist");
 
 window.addEventListener("resize",()=>{
     if(initial_hight>window.innerHeight){
-        alert("タッチキーボードを検出しました！")
+        assist_box.style.display="block";
     }else {
         initial_hight=window.innerHeight;
-        alert("タッチキーボードがなくなりました！")
+        assist_box.style.display="none";
     }
 });
