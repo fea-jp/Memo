5d5b0196a64a7b264b58b2130539d59529ce409e
ふぇあ <chitoseame.jp@gmail.com>
2025-07-31 22:53:15 +0900

    演算子のミスを修正

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 let initial_hight=window.innerHeight;
 
 window.addEventListener("resize",()=>{
-    if(initial_hight<window.innerHeight){
+    if(initial_hight>window.innerHeight){
         alert("タッチキーボードを検出しました！")
     }else {
         initial_hight=window.innerHeight;
